57616d0a55fb9d9bfa1d410a48a9083389d7248e
Alexander Elfimow <my.free.mail@gmx.de>
2019-06-13 22:53:05 +0200

    Added header file

---
+++ src/rdrand_func.h
@@ -0,0 +1,15 @@
+#ifdef RDRAND_FUNC_H
+#define RDRAND_FUNC_H
+
+extern "C" void rdrand_func1(size_t *pOut);
+extern "C" void rdrand_func4(size_t *pOut);
+extern "C" void rdrand_func8(size_t *pOut);
+extern "C" void rdrand_func16(size_t *pOut);
+extern "C" void rdrand_func32(size_t *pOut);
+extern "C" void rdrand_func64(size_t *pOut);
+extern "C" void rdrand_func128(size_t *pOut);
+extern "C" void rdrand_func256(size_t *pOut);
+extern "C" void rdrand_func512(size_t *pOut);
+extern "C" void rdrand_func1024(size_t *pOut);
+
+#endif
