25a835c1f8fd84060a34e900f689584aa9001e62
Alexander Elfimow <my.free.mail@gmx.de>
2020-05-26 15:44:26 +0200

    Added file for 1024 rdrands

---
+++ src/rdrand_func1024.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cstdlib>
+#include <stdexcept>
+#include <cstring>
+
+#include "rdrand_func.h"
+
+int main(int argc, char *argv[])
+try
+{
+    (void)argc;
+    (void)argv;
+
+    size_t values[1024];
+    std::memset(values, 0, sizeof(values));
+
+    rdrand_func1024(&values[0]);
+
+    for (auto v: values)
+    {
+        std::cout << std::hex << v << std::endl;
+    }
+
+    return EXIT_SUCCESS;
+}
+catch (...)
+{
+    std::cerr << "Error, exception" << std::endl;
+    return EXIT_FAILURE;
+}
